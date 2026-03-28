2e6bc5b19ac67055572304f4604448578c17c8d0
William <william@is-gs.com>
2026-03-28 21:00:44 +0700

    file error message

--- src/main/transaction/order/index.tsx
+++ src/main/transaction/order/index.tsx
@@ -34,7 +34,9 @@ export default function OrderPage() {
                 downloadFile(res);
             } else {
                 const json = await res?.json();
-                toast.error(json.message, { richColors: true })
+                //Shopee API Error: {\"error\":\"logistics.shipping_document_should_print_first\",\"message\":\"The package should print first. Detail: these orders: 260321N9H94XJ7 should print\",\"request_id\":\"e3e3e7f34e15b2d401b6e0da57c7a800:010003c503470939:000000a08e9b68b4\"}
+                const test = JSON.parse(String(json.message).substring(json.message.indexOf(': ')+1));
+                toast.error(test.message, { richColors: true })
             }
         } catch (error) {
             if (error instanceof Error) {
